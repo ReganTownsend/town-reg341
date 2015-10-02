86c6bf3b84ccd72cb95db899cd06c98bd9450b5b
Regan Townsend <townsenr17@up.edu>
2015-10-02 00:44:07 -0700

    added a submit button that allows users to look at data from past years rather than the 2013-2014

--- public/ui.js
+++ public/ui.js
@@ -1,31 +1,44 @@
 /**
  *ui.js
- * 
+ *
  * Defines functionality for instrumenting the user-interface.
- *  
+ *
  */
 
 var toggle = function() {
 	// Grab the html element with the ID “about”
-    var el = document.getElementById('about');
+	var el = document.getElementById('about');
 
-    // Add the class “show” to the element.
-    addClass(el, 'show');
+	// Add the class “show” to the element.
+	addClass(el, 'show');
 
 };
 var initialize = function() {
 	console.log('Initialize!');
-	// Grab the 'About' button element, identified by the 
+	// Grab the 'About' button element, identified by the
 	// 'about-btn' id
 	var button = document.getElementById('about-btn');
-	 
-	// From this point forward, when the button is clicked, the                                                                   
-    // toggle function shall be invoked.                                                                                           
-    button.onclick = toggle;
 
+	// From this point forward, when the button is clicked, the
+	// toggle function shall be invoked.
+	button.onclick = toggle;
+	// Grab the 'Submit' button element, identified by the
+	// 'submit-btn' id.
+	button = document.getElementById('submit-btn');
+
+	// From this point forward, when the button is clicked, the
+	// fetch function shall be invoked.
+	button.onclick = fetch;
 };
 
-// When this file is included at the bottom of the page,                                                                           
-// the js is loaded after the DOM is loaded.  It is a                                                                              
-// good time to initialize the UI elements in the page.                                                                            
+var fetch = function() {
+//grab the element year
+ var year = document.getElementById('year').value;
+ console.log(year);
+ 
+ vizController(year);
+}
+// When this file is included at the bottom of the page,
+// the js is loaded after the DOM is loaded.  It is a
+// good time to initialize the UI elements in the page.
 initialize();
